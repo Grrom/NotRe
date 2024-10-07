e816b0cdfe62d9c3363436fcffb0f01c4b18d8fa
Grrom <josejeromelalunio@gmail.com>
2024-10-07 19:46:51 +0800

    init

---
+++ run.py
@@ -0,0 +1,16 @@
+import json
+from urllib.request import Request
+
+from dotenv import load_dotenv
+from function import do_something
+
+load_dotenv()
+
+request = Request(
+    method="POST",
+    url="http://testinglang.com",
+    headers={"Content-Type": "application/json"},
+    data=json.dumps({"data": "hello-world"}),
+)
+
+do_something(request)
